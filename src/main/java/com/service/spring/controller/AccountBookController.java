855623177ed478d1c9c622fe291650e79cd288b1
LisaJeoung <jlisa9912234567890@gmail.com>
2023-05-24 00:11:51 +0900

    가계부 session

--- src/main/java/com/service/spring/controller/AccountBookController.java
+++ src/main/java/com/service/spring/controller/AccountBookController.java
@@ -1,26 +1,25 @@
 package com.service.spring.controller;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.service.spring.domain.AccountBook;
-import com.service.spring.model.AccountBookDAO;
+import com.service.spring.domain.TmtUser;
 import com.service.spring.model.AccountBookService;
 
 @RestController
@@ -34,17 +33,16 @@ public class AccountBookController {
 	@Autowired
 	private AccountBookService accountBookService;
 
-    @GetMapping()
-    public List<Map<String, Object>> getStat() throws Exception {
-    	List<Map<String, Object>> s =  accountBookService.getStat("testId");
+    @GetMapping 
+    public List<Map<String, Object>> getStat(@RequestParam("generalId") String generalId) throws Exception {
+    	List<Map<String, Object>> s =  accountBookService.getStat(generalId);
     	System.out.println(s);
     	return s;
     }
     
     @GetMapping("/detail")
-    public ResponseEntity<String> getAccountBook() throws Exception {
-    	//세션에서 가져오는 걸로 변경할 것
-    	List<AccountBook> accountBookList = accountBookService.getAccountBook("generalId1");
+    public ResponseEntity<String> getAccountBook(@RequestParam("generalId") String generalId) throws Exception {
+    	List<AccountBook> accountBookList = accountBookService.getAccountBook(generalId);
         try {
         	String jsonString = objectMapper.writeValueAsString(accountBookList);
 			return ResponseEntity.ok(jsonString);
